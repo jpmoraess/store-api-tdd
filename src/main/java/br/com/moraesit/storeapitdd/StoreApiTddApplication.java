a396315e223fa99197b411b2b1f4ac0d43a45a62
jpmoraess <moraesdeveloperweb@gmail.com>
2021-08-17 17:17:28 -0300

    initial commit

---
+++ src/main/java/br/com/moraesit/storeapitdd/StoreApiTddApplication.java
@@ -0,0 +1,13 @@
+package br.com.moraesit.storeapitdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreApiTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreApiTddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/moraesit/storeapitdd/StoreApiTddApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.moraesit.storeapitdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApiTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
