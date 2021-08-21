05e8608f0bc2661e5e50ab5b7ff135e0d3bacb66
jpmoraess <moraesdeveloperweb@gmail.com>
2021-08-21 12:35:25 -0300

    testing for the existence of a category in the repository layer

---
+++ src/test/java/br/com/moraesit/storeapitdd/domain/repositories/CategoryRepositoryTest.java
@@ -0,0 +1,52 @@
+package br.com.moraesit.storeapitdd.domain.repositories;
+
+import br.com.moraesit.storeapitdd.domain.entities.Category;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
+@DataJpaTest
+public class CategoryRepositoryTest {
+
+    @Autowired
+    TestEntityManager entityManager;
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Test
+    @DisplayName("must return true when a category with the same name entered exists in the database")
+    public void returnTrueWhenNameExists() {
+        String name = "T-shirt";
+
+        Category category = Category.builder()
+                .name("T-shirt")
+                .description("T-shirt description")
+                .build();
+
+        entityManager.persist(category);
+
+        Boolean exists = categoryRepository.existsByName(name);
+
+        assertThat(exists).isTrue();
+    }
+
+    @Test
+    @DisplayName("must return false when a category with the same name doesnt exists in the database")
+    public void returnfalseWhenNameDoesntExists() {
+        String name = "T-shirt";
+
+        Boolean exists = categoryRepository.existsByName(name);
+
+        assertThat(exists).isFalse();
+    }
+}
